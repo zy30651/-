8a234432ee3d61c6ea494ccb15e8e9cc34147cc4
ZY <zy30651@qq.com>
2019-10-16 16:52:48 +0800

    验证码序列号器的定义

---
+++ meiduo_mall/meiduo_mall/apps/verifications/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from django_redis import get_redis_connection
+
+
+class CheckImageCodeSerializer(serializers.Serializer):
+    """
+    图片验证码校验序列化器
+    """
+    image_code_id = serializers.UUIDField()
+    text = serializers.CharField(max_length=4, min_length=4)
+
+    def validate(self, attrs):
+        """
+        校验图片验证码,需要多参数同时校验
+        校验的问题：
+        """
+        # 查询redis数据库获取验证码
+        text = attrs["text"]
+        image_code = attrs['image_code_id']
+        redis_conn = get_redis_connection('verify_codes')
+        real_image_code = redis_conn.get('img_%s' % image_code)
+        # 对比
+        if real_image_code is None:
+            # 过期或不存在
+            raise serializers.ValidationError('无效图片验证码')
+        # 转换
+        real_image_code = real_image_code.decode()
+        if real_image_code.lower() != text.lower():
+            raise serializers.ValidationError('图片验证码错误')
+
+        # redis 中发送短信验证码的标志：send_flag_<mobile> : 1 60秒发送过短信，
+        mobile = self.context['view'].kwargs['mobile']
+        send_flag = redis_conn.get('send_flag_%s' % mobile)
+        if send_flag:
+            raise serializers.ValidationError('发送短信次数过于频繁')
+
+        return attrs
--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -4,6 +4,8 @@ from rest_framework.views import APIView
 from meiduo_mall.libs.captcha.captcha import captcha
 from django_redis import get_redis_connection
 from . import constants
+from rest_framework.generics import GenericAPIView
+from . import serializers
 
 
 class ImageCodeView(APIView):
@@ -16,3 +18,14 @@ class ImageCodeView(APIView):
         redis_conn.setex("img_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES, text)
 
         return HttpResponse(image, content_type="images/jpg")
+
+
+class SMSCodeView(GenericAPIView):
+    serializer_class = serializers.CheckImageCodeSerializer
+
+    def get(self, request, mobile):
+        # 校验图片码和发送短信的频次
+        # mobile 是放到了类视图中,叫kwargs中
+        serializer = self.get_serializer()
+        serializer.is_valid(raise_exception=True)
+
