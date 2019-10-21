2b62b42c5e300e917701e267fae793fc9fc3785a
ZY <zy30651@qq.com>
2019-10-21 23:11:49 +0800

    重写django认证后端，支持用户名和手机号登陆

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -218,6 +218,11 @@ JWT_AUTH = {
 # 关于Django认证系统使用的模型类
 AUTH_USER_MODEL = 'users.User'
 
+# django认证后端方法
+AUTHENTICATION_BACKENDS = [
+    'users.utils.UsernameMobileAuthBackend',
+]
+
 CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:8080',
     'http://localhost:8080',
