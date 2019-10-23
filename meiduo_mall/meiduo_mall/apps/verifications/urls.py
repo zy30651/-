a434677f19c67bf2bf37391985292940881e0624
ZY <zy30651@qq.com>
2019-10-23 18:46:27 +0800

    忘记密码第二步-发送短信

--- meiduo_mall/meiduo_mall/apps/verifications/urls.py
+++ meiduo_mall/meiduo_mall/apps/verifications/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'image_codes/(?P<image_code_id>.+)/$', views.ImageCodeView.as_view()),
     url(r'sms_codes/(?P<mobile>1[3-9]\d{9})/$', views.SMSCodeView.as_view()),
+    url(r'sms_codes/$', views.SMSCodeByToken.as_view()),
 ]
