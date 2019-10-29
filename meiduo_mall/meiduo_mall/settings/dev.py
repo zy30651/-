8d196db1e476452e60fa20b9b555919a3a236cb4
ZY <zy30651@qq.com>
2019-10-29 17:49:41 +0800

    创建areas-App，并导入area数据

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'oauth.apps.OauthConfig',
+    'areas.apps.AreasConfig',
     'verifications.apps.VerificationsConfig',
     'rest_framework'
 ]
---
+++ meiduo_mall/scripts/import_data_to_db.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mysql -h114.116.247.86 -umeiduo_mall -p19861026Zy meiduo_mall < ./areas.sql
