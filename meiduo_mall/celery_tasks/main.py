5db5a115f67fb243bb3bf0c16c10100077e79133
ZY <zy30651@qq.com>
2019-10-29 15:03:57 +0800

    定义发送邮件的异步任务

--- meiduo_mall/celery_tasks/main.py
+++ meiduo_mall/celery_tasks/main.py
@@ -1,12 +1,18 @@
 from celery import Celery
 
+# 为celery使用django配置文件进行设置
+import os
+
+if not os.getenv('DJANGO_SETTINGS_MODULE'):
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'meiduo_mall.settings.dev'
+
 celery_app = Celery('meiduo')
 
 # 导入配置文件
 celery_app.config_from_object('celery_tasks.config')
 
 # 自动注册任务
-celery_app.autodiscover_tasks(['celery_tasks.sms'])
+celery_app.autodiscover_tasks(['celery_tasks.sms', 'celery_tasks.email'])
 
 # 开启celery的命令   目录在能ls 到 celery_tasks
 # celery -A 应用路径(.包路径) worker -l info
