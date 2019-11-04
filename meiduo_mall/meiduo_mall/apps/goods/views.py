a8ee017f318ca85a07b240fb5c023e24b8ff11f2
ZY <zy30651@qq.com>
2019-11-05 00:49:44 +0800

    17-12获取热销未完成。

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView
+from .serializers import SKUSerializer
 # Create your views here.
+
+
+class HotSKUListView(ListAPIView):
+    """返回热销数据"""
+    queryset =
+    serializer_class = SKUSerializer
+    pass
