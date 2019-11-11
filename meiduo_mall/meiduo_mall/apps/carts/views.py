339b406703b3fb926317522f9e4d11962ade35b0
ZY <zy30651@qq.com>
2019-11-11 16:34:27 +0800

    修改购物车的前后端

--- meiduo_mall/meiduo_mall/apps/carts/views.py
+++ meiduo_mall/meiduo_mall/apps/carts/views.py
@@ -33,7 +33,7 @@ class CartView(APIView):
             cart_selected = redis_conn.smembers('cart_selected_%s' % user.id)
             # 将 redis 读取的数据，进行整合，形成一个字典，与cookie解读的数据一致，方便查询
             cart_dict = {}
-            for sku_id, count in redis_cart.items:
+            for sku_id, count in redis_cart.items():
                 cart_dict[int(sku_id)] = {
                     'count': int(count),
                     'selected': sku_id in cart_selected
@@ -56,7 +56,7 @@ class CartView(APIView):
             sku.count = cart_dict[sku.id]['count']
             sku.selected = cart_dict[sku.id]['selected']
 
-        serializer = CartSKUSerializer(sku_id_list, many=True)
+        serializer = CartSKUSerializer(sku_obj_list, many=True)
         return Response(serializer.data)
 
     def post(self, request):
@@ -118,8 +118,53 @@ class CartView(APIView):
             response.set_cookie('cart', cookie_cart)
             return response
 
-    # def put(self):
-    #     pass
-    #
+    def put(self, request):
+        serializer = CartSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        sku_id = serializer.validated_data.get('sku_id')
+        count = serializer.validated_data.get('count')
+        selected = serializer.validated_data.get('selected')
+
+        # 判断用户是否登录
+        try:
+            user = request.user
+        except Exception:
+            # 前端携带了错误JWT token，用户未登录
+            user = None
+
+        if user is not None and user.is_authenticated:
+            redis_conn = get_redis_connection('cart')
+            pl = redis_conn.pipeline()
+            pl.hset('cart_%s' % user.id, sku_id, count)
+            if selected:
+                # 勾选添加记录
+                pl.sadd('cart_selected_%s' % user.id, sku_id)
+            else:
+                # 删除记录
+                pl.srem('cart_selected_%s' % user.id, sku_id)
+            pl.execute()
+            return Response(serializer.data)
+        else:
+            cart_str = request.COOKIES.get('cart')
+            # 需要base64解码，但是base64解码只接受字节类型，所以需要先encode,然后结果还是字节类型
+            # 再pickle解析
+            if cart_str:
+                cart_dict = pickle.loads(base64.b64decode(cart_str.encode()))
+            else:
+                cart_dict = {}
+
+            if sku_id in cart_dict:
+                cart_dict[sku_id] = {
+                    'count': count,
+                    'selected': selected
+                }
+
+            cookie_cart = base64.b64encode(pickle.dumps(cart_dict)).decode()
+
+            response = Response(serializer.data)    # 不设置状态码，默认200
+            response.set_cookie('cart', cookie_cart)
+            return response
+
     # def delete(self):
     #     pass
